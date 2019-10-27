222197f5b72f170d1ca00c4a4ba38ad006db37dd
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-10-27 21:06:16 +0900

    달 이미지 사진 변경

--- app/src/main/java/com/example/moonapplication/MainActivity.java
+++ app/src/main/java/com/example/moonapplication/MainActivity.java
@@ -67,21 +67,13 @@ public class MainActivity extends AppCompatActivity {
 
         switch (lunAge){
             case "0.1": case "1.1": case "0.4": case "1.4": case "2.4":
-                imageView.setImageResource(R.drawable.darkmoon);
             case "3.4": case "4.4": case "5.4": case "6.4": case "7.4": case "8.4":
-                imageView.setImageResource(R.drawable.crecentmoon);
             case "9.4":
-                imageView.setImageResource(R.drawable.youngmoon);
             case "10.4": case "11.4": case "12.4": case "13.4": case "14.4":
-                imageView.setImageResource(R.drawable.nonamemoon2);
             case "15.4":
-                imageView.setImageResource(R.drawable.fullmoon);
             case "16.4": case "17.4": case "18.4": case "19.4": case "20.4": case "21.4": case "22.4":
-                imageView.setImageResource(R.drawable.nonamemoon);
             case "23.4":
-                imageView.setImageResource(R.drawable.oldmoon);
             case "24.4": case "25.4": case "26.4" : case "26.7": case "27.4": case "28.4":
-                imageView.setImageResource(R.drawable.darkmoon);
         }
     }
 
