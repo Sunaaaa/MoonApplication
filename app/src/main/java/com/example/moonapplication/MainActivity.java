c4cde091ae36d89064da2de93bef48ed26ecff9f
Sunaaaa <39547788+Sunaaaa@users.noreply.github.com>
2019-10-27 23:39:27 +0900

    월령 데이터 정제

--- app/src/main/java/com/example/moonapplication/MainActivity.java
+++ app/src/main/java/com/example/moonapplication/MainActivity.java
@@ -59,22 +59,31 @@ public class MainActivity extends AppCompatActivity {
 
         Log.i("result : ", lunAge);
 
-        setImgSrc(lunAge);
+        double lunAgeDouble = Math.floor(Double.parseDouble(lunAge));
+        int num = (int)lunAgeDouble;
+        String lunStr = String.valueOf(num);
+
+        Log.i("split ::  ", lunAgeDouble + "");
+        Log.i("split ::  ", num + "");
+        Log.i("split ::  ", lunStr + "");
+
+        setImgSrc(lunStr);
 
     }
 
-    public void setImgSrc(String lunAge) {
+    public void setImgSrc(String lunStr) {
 
-        switch (lunAge){
-            case "0.1": case "1.1": case "0.4": case "1.4": case "2.4":
-            case "3.4": case "4.4": case "5.4": case "6.4": case "7.4": case "8.4":
-            case "9.4":
-            case "10.4": case "11.4": case "12.4": case "13.4": case "14.4":
-            case "15.4":
-            case "16.4": case "17.4": case "18.4": case "19.4": case "20.4": case "21.4": case "22.4":
-            case "23.4":
-            case "24.4": case "25.4": case "26.4" : case "26.7": case "27.4": case "28.4":
-        }
+        Toast.makeText(getApplicationContext(), lunStr, Toast.LENGTH_SHORT).show();
+//        switch (lunStr){
+//            case "0.1": case "1.1": case "0.4": case "1.4": case "2.4":
+//            case "3.4": case "4.4": case "5.4": case "6.4": case "7.4": case "8.4":
+//            case "9.4":
+//            case "10.4": case "11.4": case "12.4": case "13.4": case "14.4":
+//            case "15.4":
+//            case "16.4": case "17.4": case "18.4": case "19.4": case "20.4": case "21.4": case "22.4":
+//            case "23.4":
+//            case "24.4": case "25.4": case "26.4" : case "26.7": case "27.4": case "28.4":
+//        }
     }
 
 }
